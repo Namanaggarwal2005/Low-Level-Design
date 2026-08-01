44fb3ea093c41de1c24c42b7de9ab2a0cb737bc2
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-08-01 12:19:06 +0530

    Dynamic Polymorphism added

---
+++ dynamicPolymorphism.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void speak() {
+        cout << "Animal speaks\n";
+    }
+};
+
+class Dog : public Animal {
+public:
+    void speak() override {
+        cout << "Dog barks\n";
+    }
+};
+
+class Cat : public Animal {
+public:
+    void speak() override {
+        cout << "Cat meows\n";
+    }
+};
+
+int main() {
+    Animal* a1 = new Dog();
+    Animal* a2 = new Cat();
+
+    a1->speak();
+    a2->speak();
+
+    delete a1;
+    delete a2;
+}
