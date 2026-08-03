72a0d8606db94d573906134e553aebc8ad374765
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-08-03 11:02:56 +0530

    SOLID 1

---
+++ SingleResponsibilityPrinciple.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+
+class Product{
+    public:
+    int price;
+    string name;
+
+    
+        Product(int price, string name){
+            this->price = price;
+            this->name  = name;
+        }
+};
+
+
+class ShoppingCart{
+    vector<Product*> products;
+    public:
+        void addProduct(Product* myProduct){
+            products.push_back(myProduct);
+        }
+        int calculateTotal(){
+            int sum = 0;
+            for(int i=0; i<products.size(); i++){
+                sum += products[i]->price;
+            }
+            return sum;
+        }
+};
+
+class ShoppingCartSaveToDB{
+    ShoppingCart* myCart;
+    public:
+        ShoppingCartSaveToDB(ShoppingCart* myCart){
+            this->myCart = myCart;
+        }
+        void saveToDB(){
+            // Save to DB Logic
+        }
+};
+
+class ShoppingCartInvoice{
+    public:
+        void printInvoice(ShoppingCart* myCart){
+            // Print invoice
+        }
+};
+
+
+int main(){
+
+    return 0;
+}
+
