1d9800bad7d8e91eed8187f773bb2b10f60dba27
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-08-03 11:20:07 +0530

    changes done SOLID 2

--- OpenClosePrinciple.cpp
+++ OpenClosePrinciple.cpp
@@ -1,76 +1,187 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
-
-
-class Product{
-    public:
-    int price;
+//--------------------------------------------
+// Product
+//--------------------------------------------
+class Product {
+private:
     string name;
+    int price;
 
-    
-        Product(int price, string name){
-            this->price = price;
-            this->name  = name;
-        }
+public:
+    Product(const string& name, int price)
+        : name(name), price(price) {}
+
+    string getName() const {
+        return name;
+    }
+
+    int getPrice() const {
+        return price;
+    }
 };
 
-
-class ShoppingCart{
+//--------------------------------------------
+// Shopping Cart
+//--------------------------------------------
+class ShoppingCart {
+private:
     vector<Product*> products;
-    public:
-        void addProduct(Product* myProduct){
-            products.push_back(myProduct);
-        }
-        int calculateTotal(){
-            int sum = 0;
-            for(int i=0; i<products.size(); i++){
-                sum += products[i]->price;
-            }
-            return sum;
-        }
+
+public:
+    void addProduct(Product* product) {
+        products.push_back(product);
+    }
+
+    int calculateTotal() const {
+        int sum = 0;
+
+        for (auto product : products)
+            sum += product->getPrice();
+
+        return sum;
+    }
+
+    const vector<Product*>& getProducts() const {
+        return products;
+    }
 };
 
-class SaveToDB{
-    ShoppingCart * cart;
-    public:
-        virtual void save() = 0;
-
+//--------------------------------------------
+// Repository Interface
+//--------------------------------------------
+class CartRepository {
+public:
+    virtual void save(const ShoppingCart& cart) = 0;
+    virtual ~CartRepository() = default;
 };
 
-class SaveToSQL : public SaveToDB{
-    public:
-        void save(){
-            // Save to SQL
+//--------------------------------------------
+// SQL Repository
+//--------------------------------------------
+class SQLRepository : public CartRepository {
+public:
+    void save(const ShoppingCart& cart) override {
+
+        cout << "\nSaving Cart to SQL Database\n";
+
+        for (auto product : cart.getProducts()) {
+            cout << product->getName()
+                 << " : Rs. "
+                 << product->getPrice()
+                 << endl;
         }
+
+        cout << "Total = Rs. "
+             << cart.calculateTotal()
+             << endl;
+    }
 };
 
-class SaveToMongo : public SaveToDB{
-    public:
-        void save(){
-            // Save to Mongo
+//--------------------------------------------
+// Mongo Repository
+//--------------------------------------------
+class MongoRepository : public CartRepository {
+public:
+    void save(const ShoppingCart& cart) override {
+
+        cout << "\nSaving Cart to MongoDB\n";
+
+        for (auto product : cart.getProducts()) {
+            cout << product->getName()
+                 << " : Rs. "
+                 << product->getPrice()
+                 << endl;
         }
+
+        cout << "Total = Rs. "
+             << cart.calculateTotal()
+             << endl;
+    }
 };
 
-class SaveToFile : public SaveToDB{
-    public:
-        void save(){
-            // Save to File
+//--------------------------------------------
+// File Repository
+//--------------------------------------------
+class FileRepository : public CartRepository {
+public:
+    void save(const ShoppingCart& cart) override {
+
+        cout << "\nSaving Cart to File\n";
+
+        for (auto product : cart.getProducts()) {
+            cout << product->getName()
+                 << " : Rs. "
+                 << product->getPrice()
+                 << endl;
         }
+
+        cout << "Total = Rs. "
+             << cart.calculateTotal()
+             << endl;
+    }
 };
 
-class ShoppingCartInvoice{
-    public:
-        void printInvoice(ShoppingCart* myCart){
-            // Print invoice
+//--------------------------------------------
+// Invoice Printer
+//--------------------------------------------
+class ShoppingCartInvoice {
+public:
+    void printInvoice(const ShoppingCart& cart) {
+
+        cout << "\n========== INVOICE ==========\n";
+
+        for (auto product : cart.getProducts()) {
+            cout << product->getName()
+                 << " : Rs. "
+                 << product->getPrice()
+                 << endl;
         }
+
+        cout << "-----------------------------\n";
+        cout << "Total : Rs. "
+             << cart.calculateTotal()
+             << endl;
+
+        cout << "=============================\n";
+    }
 };
 
+//--------------------------------------------
+// Main
+//--------------------------------------------
+int main() {
 
-int main(){
+    Product p1("Laptop", 60000);
+    Product p2("Mouse", 1500);
+    Product p3("Keyboard", 2500);
+
+    ShoppingCart cart;
+
+    cart.addProduct(&p1);
+    cart.addProduct(&p2);
+    cart.addProduct(&p3);
+
+    ShoppingCartInvoice invoice;
+    invoice.printInvoice(cart);
+
+    CartRepository* repository;
+
+    repository = new SQLRepository();
+    repository->save(cart);
+    delete repository;
+
+    repository = new MongoRepository();
+    repository->save(cart);
+    delete repository;
+
+    repository = new FileRepository();
+    repository->save(cart);
+    delete repository;
 
     return 0;
 }
-
